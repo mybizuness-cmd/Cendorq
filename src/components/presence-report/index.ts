50361dd8c928ee480b7fcd642275434b2d3f6ff0
mybizuness-cmd <mybizuness@gmail.com>
2026-05-20 21:07:23 -0700

    Export Presence Report next move policy panel

--- src/components/presence-report/index.ts
+++ src/components/presence-report/index.ts
@@ -1,4 +1,5 @@
 export { PresenceReportEvidenceBoundaryPanel } from "./presence-report-evidence-boundary-panel";
+export { PresenceReportNextMovePolicyPanel } from "./presence-report-next-move-policy-panel";
 export { PresenceReportPreview } from "./presence-report-preview";
 export { ProtectedFreeScanResultPreview } from "./protected-free-scan-result-preview";
 export { SamplePresenceReport } from "./sample-presence-report";
