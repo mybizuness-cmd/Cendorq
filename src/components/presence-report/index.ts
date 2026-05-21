606381fbb58d424ef316ac0d57261cfa2ef67403
mybizuness-cmd <mybizuness@gmail.com>
2026-05-20 17:52:22 -0700

    Export vertical Sample Presence Report component

--- src/components/presence-report/index.ts
+++ src/components/presence-report/index.ts
@@ -1,3 +1,4 @@
 export { PresenceReportPreview } from "./presence-report-preview";
 export { ProtectedFreeScanResultPreview } from "./protected-free-scan-result-preview";
 export { SamplePresenceReport } from "./sample-presence-report";
+export { VerticalSamplePresenceReport } from "./vertical-sample-presence-report";
